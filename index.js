e3f70a5311b50a6d66931e9acfe2bf506773b269
vigneshajith <112766532+vigneshajith@users.noreply.github.com>
2022-12-06 10:44:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ var thanks = document.querySelector(".thanks");
 var submit = document.querySelector("#submitbtn");
 var rating = document.querySelector("#user__rating");
 var ratebtn = document.querySelectorAll(".num__circle");
-submit.addEventListener("click", () => {
-    thanks.classList.remove("hidden");
-    container.style.cssText = "display:none";
+
 })
 ratebtn.forEach((ratebtn) => {
     ratebtn.addEventListener("click", () => {
         rating.innerHTML = ratebtn.innerHTML; 
+        submit.addEventListener("click", () => {
+    thanks.classList.remove("hidden");
+    container.style.cssText = "display:none";
+        
     });
     
 });
